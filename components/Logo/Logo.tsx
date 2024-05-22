cfe4cbd84f6053ada10beee2f8ca140504f8d0e2
Michael <enjoyweaver@gmail.com>
2024-05-22 13:23:30 -0500

    eslint

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,24 +1,19 @@
 import clsx from "clsx";
 import { ComponentProps } from "react";
 import styles from "./Logo.module.css";
+import Image from "next/image";
+import icon from "@/app/banner.jpg"; // Make sure the path is correct
 
 export function Logo({ className, ...props }: ComponentProps<"div">) {
   return (
     <div className={clsx(className, styles.logo)} {...props}>
-      <svg
-        className={styles.mark}
-        fill="none"
-        viewBox="0 0 32 32"
-        xmlns="https://twitter.com/Xogos_Gaming/header_photo"
-      >
-        <path
-          clipRule="evenodd"
-          d="M21.657 8H2l5.657 5.6v7.733L21.657 8ZM10.343 24H30l-5.657-5.6v-7.733L10.343 24Z"
-          fill="currentColor"
-          fillRule="evenodd"
-        />
-      </svg>
-      <span className={styles.wordmark}>Xogos Gaming</span>
+      <Image
+        src={icon}
+        alt="Xogos Gaming Logo"
+        className={styles.icon}
+        width={110}
+        height={42}
+      />
     </div>
   );
 }
