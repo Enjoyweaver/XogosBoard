da68d6fe3432cc7138406f620b5d5ce437eb09ff
Michael <enjoyweaver@gmail.com>
2024-05-22 13:07:48 -0500

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,8 @@ module.exports = {
     "react/display-name": "off",
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
-
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
     "sort-imports": ["error", { ignoreDeclarationSort: true }],
     "import/default": "off",
     "import/export": "warn",
