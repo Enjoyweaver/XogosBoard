ff92f567e60f95f2dac85debf39a6b5133ae0bb0
Michael <enjoyweaver@gmail.com>
2024-05-22 13:28:11 -0500

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <html lang="en">
+      <head>
+        <title>Xogos Board</title>
+        <link rel="icon" href="/app/icon.jpg" type="image/jpeg" />
+      </head>
       <body className={inter.className}>
         <Providers session={session}>{children}</Providers>
       </body>
